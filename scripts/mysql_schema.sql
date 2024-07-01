c669dd9d81ec4023df15e3b27b645125d79e440e
huypl53 <phamlehuy52@gmail.com>
2024-07-01 14:27:05 +0700

    feat: sql formatter

--- scripts/mysql_schema.sql
+++ scripts/mysql_schema.sql
@@ -1,4 +1,41 @@
-CREATE TABLE `aes`.`user` (
-  `id` INT NOT NULL,
-  `name` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+INSERT INTO
+  `aes`.`mastery_bkt_multi_learn` (
+    `user_id`,
+    `skill_id`,
+    `pl`,
+    `updated_at`,
+    `description`
+  )
+VALUES
+  (
+    2,
+    10,
+    0.5,
+    '2024-06-30 16:30:30',
+    'latest BKT multilearn'
+  );
+
+INSERT INTO
+  `aes`.`bkt_params` (
+    `create_at`,
+    `modified_at`,
+    `model_type`,
+    `active`,
+    `skill_id`,
+    `ppl`,
+    `ps`,
+    `pt`,
+    `pg`
+  )
+VALUES
+  (
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP,
+    2,
+    2,
+    10,
+    0.7,
+    0.2,
+    0.6,
+    0.1
+  );
